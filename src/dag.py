93739714128eeae2e8503aee409bc0dc1a7c0118
Isaac Wolford <cybergeek.1943@gmail.com>
2025-07-27 16:38:28 -0600

    Enhanced the DAG class.

--- src/dag.py
+++ src/dag.py
@@ -1,5 +1,5 @@
-from typing import Callable, Any, Union, Sequence, Generator, Self
-from math import e, comb
+from typing import Callable, Any, Union, Sequence, Generator, Self, Iterator
+from math import comb, ceil
 
 
 def nCk(n: int, k: int) -> int:  # Alias
@@ -8,7 +8,6 @@ def nCk(n: int, k: int) -> int:  # Alias
     return comb(n, k)
 
 
-type LinearFunction = Callable[[float, ...], float]
 class Funcs:
     sum = lambda *args: sum([*args])
     lf_1 = lambda x1, x2, x3: 2 * (x1 + x2) + 3 * x3 - 10
@@ -49,26 +48,23 @@ class Level(list):
 
     def multihead_loop(self, heads: Sequence[int] = (0, 1),
                        step: Sequence[int] = (1, 1),
-                       start_pos: int = 0, end_pos: int = -1) -> Generator[Sequence[int], None, None]:
+                       repeat_step: int = -1) -> Generator[Sequence[int], None, None]:
         if len(heads) != len(step):
-            raise IndexError('There must be the same number of `heads` as number of `steps`')
-        if end_pos == -1:
+            raise IndexError('There must be the same number of `heads` as number of `step`')
+        if repeat_step == -1:
             if self.f:
-                raise ValueError('When level is a continuous function, end_pos must be specified (other than -1).')
+                raise ValueError('When level is a continuous function, `repeat` must be specified (other than -1).')
             else:
-                end_pos = len(self)
-        if start_pos != 0:
-            heads = [h + start_pos for h in heads]
-        for _ in range(start_pos, end_pos):
+                repeat_step = ceil(len(self) / min(step)) - ceil(min(heads) / min(step))
+        for _ in range(abs(repeat_step)):
             yield [self[h] for h in heads]
             heads = [h + s for h, s in zip(heads, step)]
 
 
 class DAG:
-    def __init__(self, as_continuous: Callable[[int, float], Any] | bool = None,
-                 **kwargs) -> None:
+    def __init__(self, as_continuous: Callable[[int, float], Any] | bool = None, **kwargs) -> None:
         self.levels: dict[int, Level] = {}
-        if 'basin' in kwargs and 'depth' in kwargs:
+        if kwargs and 'basin' in kwargs and 'depth' in kwargs:
             self.as_graph(**kwargs)
 
         # Continuous Function Attributes
@@ -93,35 +89,43 @@ class DAG:
                  depth: int,
                  node_arity: int | Sequence[int] = 2,  # also known as heads
                  step: int | Sequence[int] = 1,
-                 start_pos: int = None,
-                 end_pos: int = None,
+                 repeat_step: int | Callable[[Any, ...], int] = None,
                  save_levels: bool = True,
-                 func: LinearFunction = Funcs.sum,
+                 func: Callable[..., int | float | Sequence[int | float]] = Funcs.sum,
                  step_caller: Callable[[Level], Any] = None) -> Self:
         self.clear_graph()
         if not isinstance(basin, Level):
             basin: Level = Level(list(basin))
-        if len(basin) == 1 and basin[0] == 1 and node_arity == 2 and func == Funcs.sum:
+        if (len(basin) == 1 and
+                basin[0] == 1 and
+                node_arity == 2 and
+                step == 1 and
+                repeat_step is None and
+                func == Funcs.sum):
             return self.__generate_pascals(depth, save_levels, step_caller)
-        self.levels[0] = basin
         # Set up params for multihead loop
-        if start_pos is None:
-            start_pos = 0
+        self.levels[0] = basin
         if isinstance(node_arity, int):
             node_arity = [p for p in range(-node_arity + 1, 1)]
         if isinstance(step, int):
             step = [step for _ in range(len(node_arity))]
-        print(node_arity, step)
-        return self.__generate_graph(basin, depth, node_arity, step, start_pos, end_pos, save_levels, func, step_caller)
+        if repeat_step is None:
+            _min_head: int = min(node_arity)
+            _min_step: int = min(step)
+            repeat_step = lambda level, heads, step, level_idx: ceil(len(level) / _min_step) - ceil(_min_head / _min_step)
+        elif isinstance(repeat_step, int):
+            _repeat_step: int = repeat_step
+            repeat_step = lambda *args: _repeat_step
+        self.__generate_graph(basin, depth, node_arity, step, repeat_step, save_levels, func, step_caller)
+        return self
 
     def __generate_graph(self, l: Level,
                          depth: int,
                          heads: Sequence[int],
                          step: Sequence[int],
-                         start_pos: int,
-                         end_pos: int | None,
+                         repeat_step: Callable[[Level, Sequence[int], Sequence[int], int], int],
                          save_levels: bool,
-                         func: LinearFunction,
+                         func: Callable[..., int | float | Sequence[int | float]],
                          step_caller: Callable[[Level], Any],
                          __level_idx: int = 0) -> Self:
         """Recursive DAG generator."""
@@ -130,14 +134,21 @@ class DAG:
         if __level_idx == depth:
             if not save_levels: self.levels[__level_idx] = l
             return self
-        new_level = Level()
-        for _ in l.multihead_loop(heads, step, start_pos, end_pos=len(l) + len(heads) - 1 if end_pos is None else end_pos):
-            new_level.append(func(*_))
-        # for idx in range(-node_arity + 1, len(l)):  # NOTE: this is old code with less flexibility as multihead_loop()
+        new_level: Level = Level()
+        for args in l.multihead_loop(heads, step, repeat_step=repeat_step(l, heads, step, __level_idx)):
+            nv: int | float | Sequence[int | float] = func(*args)
+            if isinstance(nv, Sequence):
+                new_level.extend(nv)
+            else:
+                new_level.append(nv)
+        # NOTE: this is old code with less flexibility as multihead_loop():
+        # for idx in range(-node_arity + 1, len(l)):
         #     new_level.append(func(*[l[i] for i in range(idx, idx + node_arity)]))
         if save_levels:
             self.levels[__level_idx + 1] = new_level
-        return self.__generate_graph(new_level, depth, heads, step, start_pos, end_pos, save_levels, func, step_caller, __level_idx + 1)
+
+        # Recursive Call
+        return self.__generate_graph(new_level, depth, heads, step, repeat_step, save_levels, func, step_caller, __level_idx + 1)
 
     def __generate_pascals(self, depth: int, save_levels: bool, step_caller: Callable[[Level], Any]) -> Self:
         """Optimization in the event the Basin=[1]"""
@@ -149,9 +160,16 @@ class DAG:
         self.levels[depth] = Level([nCk(depth, k) for k in range(depth+1)])
         return self
 
+    def set_level_kwargs(self, **kwargs) -> Self:
+        for l in self.levels.values():
+            for k, v in kwargs.items():
+                setattr(l, k, v)
+        return self
+
     def get_diagonal(self, start_idx: int,
                      x_step: Callable[[int], int] | int = 1,
-                     y_step: Callable[[int], int] | int = -1) -> Level:
+                     y_step: Callable[[int], int] | int = -1,
+                     repeat_step: int = None) -> Level:
         """
         Useful if we want to get a diagonal to analyze the coeffs of Chebyshev Polynomials, Fibonacci polynomials, etc.
         """
@@ -163,24 +181,37 @@ class DAG:
             y_step = lambda y: y + _y
         out: list[int | float] = []
         x, y = 0, start_idx  # current positions
-        while y != -1 and (v:=(self.get_level(y)[x])) != 0:
-            out.append(v)
-            x = x_step(x)
-            y = y_step(y)
+
+        if repeat_step:
+            for _ in range(repeat_step):
+                out.append(self.get_level(y, default=0)[x])
+                x = x_step(x)
+                y = y_step(y)
+        else:
+            while y != -1 and (v:=(self.get_level(y, default=0)[x])) != 0:
+                out.append(v)
+                x = x_step(x)
+                y = y_step(y)
         return Level(out)
 
-    def get_level(self, m: int, **kwargs) -> Level:
+    def get_level(self, m: int, default: Level | Any = ...) -> Level:
         if m == -1:
             if not self.levels:
                 raise IndexError('No levels exist in empty DAG')
             if self.continuous and self.f:
                 raise IndexError('Cannot determine "last" level for continuous functions')
             m = max(self.levels.keys())
-        return self.levels.get(
-            m,
-            Level((lambda x: self.f(m, x)) if self.continuous and self.f
-            else [nCk(m, k) for k in range(m + 1)], **kwargs)
-        )
+        try:
+            return self.levels[m]
+        except KeyError:
+            if default is ...:
+                if self.continuous and self.f:
+                    default: Level = Level((lambda x: self.f(m, x)))
+                else:
+                    default: Level = Level([nCk(m, k) for k in range(m + 1)])
+            elif not isinstance(default, Level):
+                default: Level = Level(obv=default)
+            return default
 
     def __getitem__(self, m: int) -> Level:  # enables getting row and column by DAG[r][c]
         return self.get_level(m)
@@ -190,6 +221,9 @@ class DAG:
 
     def __repr__(self) -> str:
         return str(self)
+
+    def __iter__(self) -> Iterator[Level]:
+        return self.levels.values().__iter__()
 
 
 # TODO Current Findings:
@@ -207,7 +241,6 @@ class DAG:
 
 
 if __name__ == '__main__':
-    import random as rd
     # The graph exhibits an Attractor Property (and Structural Invariance) to the nth root function.
     # It may explain why Newton's method, when iterated symbolically, yields the observed binomial patterns - both are manifestations of the same underlying attractor.
     # start = Level([1])  # Basin of Attraction (first level)
@@ -219,5 +252,13 @@ if __name__ == '__main__':
     # print_func(ll.getLevel(5), k=2)
     # g = DAG()
     # print(g[5][3])
-    dag = DAG(basin=[2], depth=4, node_arity=2)
+    dag = DAG().as_graph(basin=[1], depth=10, node_arity=(-2, -1, 0), step=(1, 2, 1))
     print(dag)
+    # for l in dag:
+    #     print(len(l), l)
+    # l = []
+    for r in range(10):
+        print(sum(l:=dag.get_diagonal(r, x_step=1, y_step=-1)), l)
+    # l = Level([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    # for i, j, k in l.multihead_loop((-2, -1, 0), step=(2, 2, 2), repeat_step=10 // 2 + 1):
+    #     print(i, j, k)
